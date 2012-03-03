a97485a47f66ffc6af120b86e80edf492999f1e0
as <defmethod@gmail.com>
2012-03-04 02:51:59 +0900

    first commit

---
+++ LifeGameMain.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class LifeGameMain {
+	public static void main(String[] args) {
+		LifeGame lg = new LifeGame();
+		lg.mainloop();
+	}
+}
+
